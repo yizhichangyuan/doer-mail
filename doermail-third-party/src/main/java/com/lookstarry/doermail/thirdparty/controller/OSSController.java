4289298626fd3c84cebbecd152cddb5770fac4b0
yizhichangyuan <49146565+yizhichangyuan@users.noreply.github.com>
2022-08-24 21:42:32 +0800

    Update OSSController.java

--- doermail-third-party/src/main/java/com/lookstarry/doermail/thirdparty/controller/OSSController.java
+++ doermail-third-party/src/main/java/com/lookstarry/doermail/thirdparty/controller/OSSController.java
@@ -98,9 +98,9 @@ public class OSSController {
         String host = "https://msg.market.alicloudapi.com";
         String path = "/msg";
         String method = "POST";
-        String appcode = "c1774f94a64c432097cc01196c313501";
+        String appcode = "#####";
         Map<String, String> headers = new HashMap<String, String>();
-        //最后在header中的格式(中间是英文空格)为Authorization:APPCODE 83359fd73fe94948385f570e3c139105
+        //最后在header中的格式(中间是英文空格)为Authorization:APPCODE ####
         headers.put("Authorization", "APPCODE " + appcode);
         //根据API的要求，定义相对应的Content-Type
         headers.put("Content-Type", "application/x-www-form-urlencoded; charset=UTF-8");
